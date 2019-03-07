1efb21146b99088c9a8c9719fa4ef4511d191087
R <rkkoszewski@gmail.com>
2019-03-07 12:05:45 +0000

    Add Setup Script for Debian - DietPi

---
+++ setup-debian.py
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Setup Script to be used in Debian (Tested with DietPi) - Requires sudo to be installed
+
+# Install Required Packages
+sudo apt-get update
+sudo apt-get install lxc python python-lxc python-flask python-setuptools curl
+sudo apt-get install python-dev libldap2-dev libsasl2-dev libffi-dev libssl-dev
+
+# Install Node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get update
+sudo apt-get install build-essential nodejs
+
+# Build Files
+cd jsbuild
+npm install
+node_modules/.bin/bower install --allow-root
+node_modules/.bin/grunt --allow-root
+cd ..
+
+# Install Python Package
+sudo python setup.py install
+
+# Copy Configuration Files
+sudo mkdir /etc/lwp
+sudo cp lwp.example.conf /etc/lwp/lwp.conf
+sudo mkdir /var/lwp
+sudo cp lwp.db /var/lwp/lwp.db
+
+# DONE
+echo "SETUP FINISHED. Start LWP with command 'sudo lwp'"
